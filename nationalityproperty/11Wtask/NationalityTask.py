3963aee69f9b24881b474de36554e6f8f1ee19cd
unknown <Gutentag0004@gmail.com>
2021-06-26 00:40:34 +0800

    modify the match pattern

--- nationalityproperty/11Wtask/NationalityTask.py
+++ nationalityproperty/11Wtask/NationalityTask.py
@@ -2,54 +2,70 @@ import pypinyin
 import requests
 import bs4
 import time
-import csv
 
 
 
-def getTargetNation(name, Nationslist):
-    resultlist =[]
-    url = "http://s.tcmap.com.cn/cse/search?q={}&click=1&s=8921814902502906757&nsid="
-
-    target = url.format(name)
-    result =  requests.get(target)
-    print("resut"+str(result.status_code))
-    if(result.status_code != 200):
-        print(name)
-        print("request fail")
-        return resultlist
-    co = result.content
-    soup = bs4.BeautifulSoup(co)
-    list = soup.find_all('a', target='_blank')
-    if (len(list) > 0):
-        hre = list[0]
-        print(hre)
-        destination = requests.get(hre['href'])
-        print("status"+str(destination.status_code))
-        if (destination.status_code != 200):
-            print(name)
-            print("request fail")
-            return resultlist
-        soup = bs4.BeautifulSoup(destination.content)
-        contentStr = str(soup)
-        print(contentStr)
-        for nation in Nationslist:
-            if nation in contentStr:
-                resultlist.append(nation)
-
-    return resultlist
-nationsListStr = open("D:\PycharmProjects\gmyproj\\nationalityproperty\\nationslist",'r',encoding="utf-8")
-
-
+# 民族列表路径
+nationsListPath = "C:\\Users\\gph\\workspace\\NishinoNanase\\nationalityproperty\\nationslist"
+# 社区列表路径
+community = "D:\\PycharmProjects\\gmyproj\\nationalityproperty\\11Wtask\\community.txt"
+# 村委会路径
+villageCommunity = "C:\\Users\\gph\\workspace\\NishinoNanase\\nationalityproperty\\11Wtask\\villageCommunity.txt"
+# 获取民族成分之后的结果路径
+areasWithNationality = "C:\\Users\\gph\\workspace\\NishinoNanase\\nationalityproperty\\11Wtask\\areasWithNationality.txt"
+nationsListStr = open(nationsListPath,'r',encoding="utf-8")
 nationsList = []
+
 #获取民族列表
 for line in nationsListStr.readlines():
     nationsList.append(line.strip());
 print(nationsList)
 
-community = "D:\PycharmProjects\gmyproj\\nationalityproperty\\11Wtask\\community.txt"
-villageCommunity = "D:\PycharmProjects\gmyproj\\nationalityproperty\\11Wtask\\villageCommunity.txt"
-areasWithNationality = "D:\PycharmProjects\gmyproj\\nationalityproperty\\11Wtask\\areasWithNationality.txt"
-testFile = "D:\PycharmProjects\gmyproj\\nationalityproperty\\11Wtask\\test.txt"
+# function  request请求 获取民族列表
+def getTargetNation(name, Nationslist):
+    print("request")
+    resultlist =[]
+    url = "http://s.tcmap.com.cn/cse/search?q={}&click=1&s=8921814902502906757&nsid="
+
+    target = url.format(name)
+    result = ""
+    try:
+        result = requests.get(target, timeout=5)
+    except BaseException:
+        print('timeout exception')
+        return resultlist
+    else:
+        print("resut"+str(result.status_code))
+        if(result.status_code != 200):
+            print(name)
+            print("request fail")
+            return resultlist
+        co = result.content
+        soup = bs4.BeautifulSoup(co)
+        list = soup.find_all('a', target='_blank')
+        if (len(list) > 0):
+            hre = list[0]
+            print(hre)
+            destination = requests.get(hre['href'])
+            print("status"+str(destination.status_code))
+            if (destination.status_code != 200):
+                print(name)
+                print("request fail")
+                return resultlist
+            soup = bs4.BeautifulSoup(destination.content)
+            contentStr = str(soup)
+            for nation in Nationslist:
+                if nation[:-1] in contentStr:
+                    resultlist.append(nation)
+
+
+    return resultlist
+
+
+
+
+
+# main function 根据input文件  获取对应文件中对应地址的民族成分输出到outPut中
 def getNationlityFunc(inputName, outPutName):
     count = 0
     up = 0
@@ -85,12 +101,11 @@ def getNationlityFunc(inputName, outPutName):
             if len(propertylist) > 0:
                 up +=1
     print(str(up)+"/"+str(down))
-
+    output.flush()
     input.close()
     output.close()
 
 
 
 #getNationlityFunc(community,areasWithNationality)
-# getNationlityFunc(villageCommunity,areasWithNationality)
-getNationlityFunc(testFile,areasWithNationality)
+getNationlityFunc(villageCommunity,areasWithNationality)
